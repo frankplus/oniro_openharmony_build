69cd2cd18d18e6ef0632acd75aba9ef8e61223b1
weichaox <wangweichao2@huawei.com>
2021-09-09 20:41:58 +0800

    build ohos-sdk on mac
    Signed-off-by: weichaox <wangweichao2@huawei.com>
    Change-Id: I4b8e87c8d6ce87305819f8003799b6f13e384d1a

--- misc/mac/find_sdk.py
+++ misc/mac/find_sdk.py
@@ -69,13 +69,12 @@ def main():
     # command-line tools must be installed. Stand-alone installations (in
     # /Library/Developer/CommandLineTools) are not supported.
     # https://bugs.chromium.org/p/chromium/issues/detail?id=729990#c1
-
     file_path = os.path.relpath("/path/to/Xcode.app")
     if not os.path.isdir(sdk_dir) or not '.app/Contents/Developer' in sdk_dir:
         raise SdkError('Install Xcode, launch it, accept the license ' +
                        'agreement, and run `sudo xcode-select -s %s` ' % file_path +
                        'to continue.')
-    sdks = [re.findall('^MacOSX(10\.\d+)\.sdk$', s) for s in
+    sdks = [re.findall('^MacOSX(1[0,1]\.\d+)\.sdk$', s) for s in
             os.listdir(sdk_dir)]
     sdks = [s[0] for s in sdks if s]  # [['10.5'], ['10.6']] => ['10.5', '10.6']
     sdks = [s for s in sdks  # ['10.5', '10.6'] => ['10.6']
