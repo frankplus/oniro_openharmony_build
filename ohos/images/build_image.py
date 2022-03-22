9bddfef8679e676a3f74384a55bfb41567d4af5c
handyohos <zhangxiaotian@huawei.com>
2022-03-22 09:57:32 +0800

    bugfix: change sys-prod/chip-prod to sys_prod/chip_prod.
    1）修改build仓，把根文件系统中的sys-prod/chip-prod修改为sys_prod/chip_prod。
    2）修改selinux仓，把文件夹标签中的名称也相应修改。
    
    Signed-off-by: handyohos <zhangxiaotian@huawei.com>
    Change-Id: I569f8c2802c22ddef55341452c5a331d699f85cb
    
    #I4YY38

--- ohos/images/build_image.py
+++ ohos/images/build_image.py
@@ -38,7 +38,7 @@ def _prepare_root(system_path, target_cpu):
     os.makedirs(root_dir, exist_ok=True)
     _dir_list = [
         'config', 'dev', 'proc', 'sys', 'updater', 'system', 'vendor', 'data',
-        'chipset', 'storage', 'mnt', 'tmp', 'sys-prod', 'chip-prod'
+        'chipset', 'storage', 'mnt', 'tmp', 'sys_prod', 'chip_prod'
     ]
     for _dir_name in _dir_list:
         os.makedirs(os.path.join(root_dir, _dir_name), exist_ok=True)
