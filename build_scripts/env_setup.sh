6f629068ad9ac7cc682b32373f04d15e546c8f38
zwx1138075 <zhuyunping@huawei.com>
2022-06-22 18:46:30 +0800

    缩进错误修复
    Signed-off-by: zwx1138075 <zhuyunping@huawei.com>

--- build_scripts/env_setup.sh
+++ build_scripts/env_setup.sh
@@ -23,11 +23,11 @@ sed -i "s@ENV LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8@@g" ./bui
 sed -i "s@RUN @@g" ./build/build_scripts/Dockerfile
 sed -i "s@&& @@g" ./build/build_scripts/Dockerfile
 
-sed  -i 's@rm -rf /bin/sh.*\s@@' ./build/build_scripts/Dockerfile
-sed  -i 's@ln -s /bin.*\s@@g' ./build/build_scripts/Dockerfile
-sed  -i 's@ln -s /usr.*\s@@g' ./build/build_scripts/Dockerfile
-sed  -i 's@locale-gen .*\s@@g' ./build/build_scripts/Dockerfile
-sed  -i 's@^export PATH=.*\s@@g' ./build/build_scripts/Dockerfile
+sed -i 's@rm -rf /bin/sh.*\s@@' ./build/build_scripts/Dockerfile
+sed -i 's@ln -s /bin.*\s@@g' ./build/build_scripts/Dockerfile
+sed -i 's@ln -s /usr.*\s@@g' ./build/build_scripts/Dockerfile
+sed -i 's@locale-gen .*\s@@g' ./build/build_scripts/Dockerfile
+sed -i 's@^export PATH=.*\s@@g' ./build/build_scripts/Dockerfile
 
 sed -i 's@ruby\S*\s@ruby @' ./build/build_scripts/Dockerfile
 sed -i 's@python3\S*\s@@g' ./build/build_scripts/Dockerfile
@@ -48,12 +48,12 @@ result2=$(echo $SHELL | grep "zsh")
 userhome=~
 if [[ "$result1" != "" ]]
 then
-	sed -i "s@/root/.bashrc@"$userhome"/.bashrc@g" ./build/build_scripts/Dockerfile
+    sed -i "s@/root/.bashrc@"$userhome"/.bashrc@g" ./build/build_scripts/Dockerfile
 elif [ [$result2 != ""] ]
 then
-	sed -i "s@/root/.bashrc@"$userhome"/.zshrc@g" ./build/build_scripts/Dockerfile
+    sed -i "s@/root/.bashrc@"$userhome"/.zshrc@g" ./build/build_scripts/Dockerfile
 else
-	echo "Shell is not default, please configure the PATH variable manually"
+    echo "Shell is not default, please configure the PATH variable manually"
 fi
 
 mv ./build/build_scripts/Dockerfile ./build/build_scripts/rundocker.sh
