be48998c7da443cb5685083b2256d95a4724c8a5
Caoruihong <crh.cao@huawei.com>
2022-08-02 17:55:47 +0800

    fix(build): using a more robust method to check the system shell type
    Signed-off-by: Caoruihong <crh.cao@huawei.com>
    Change-Id: Ieebeb36fe5f303aa11ca656b995612bf7afb1bd5

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -18,12 +18,11 @@ echo "++++++++++++++++++++++++++++++++++++++++"
 function check_shell_environment() {
   case $(uname -s) in 
     Linux)
-          shell_result=$(ls -l /bin/sh)
-          echo ${shell_result}
-          if [[ $shell_result =~ "-> /bin/bash" ]] || [[ $shell_result =~ "-> bash" ]];then
-            echo "The ubuntu system shell is bash"
+          shell_result=$(/bin/sh -c 'echo ${BASH_VERSION}')
+          if [ -n "${shell_result}" ]; then
+            echo "The system shell is bash ${shell_result}"
           else
-            echo -e "\033[33m The shell of your system need point to  /bin/sh -> /bin/bash or /bin/sh -> bash ,because some commands may not be supported in dash such as pushd, shopt \n You can follow the tips below modify the Ubuntu shell to bash. \033[0m"
+            echo -e "\033[33m The shell of your system must be bash, because some commands may not be supported in dash such as pushd and shopt etc. \n You can follow these tips to modify the system shell to bash on Ubuntu: \033[0m"
             echo -e "\033[33m [1]:Open the Terminal tool and execute the following command: sudo dpkg-reconfigure dash \n [2]:Enter the password and select <no>  \033[0m"
             exit 1
           fi
