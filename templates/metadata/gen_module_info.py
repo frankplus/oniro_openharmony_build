7e5c743bd1532fbd869c6a05257ed1515945026e
cheng_jinsong <chengjinsong2@huawei.com>
2022-08-16 11:31:54 +0800

    add sys_prod chip_prod images
    Signed-off-by: cheng_jinsong <chengjinsong2@huawei.com>

--- templates/metadata/gen_module_info.py
+++ templates/metadata/gen_module_info.py
@@ -59,7 +59,7 @@ def _gen_install_dest(base_dir, module_install_dir, relative_install_dir,
 
 
 def gen_install_dests(system_base_dir, ramdisk_base_dir, vendor_base_dir,
-                      updater_base_dir, source_file_name, install_images,
+                      updater_base_dir, sys_prod_base_dir, chip_prod_base_dir, source_file_name, install_images,
                       module_install_dir, relative_install_dir, module_type):
     """Generate module install dir by user config."""
     dests = []
@@ -78,6 +78,12 @@ def gen_install_dests(system_base_dir, ramdisk_base_dir, vendor_base_dir,
                                      relative_install_dir, module_type)
         elif image == 'updater':
             dest = _gen_install_dest(updater_base_dir, module_install_dir,
+                                     relative_install_dir, module_type)
+        elif image == 'sys_prod':
+            dest = _gen_install_dest(sys_prod_base_dir, module_install_dir,
+                                     relative_install_dir, module_type)
+        elif image == 'chip_prod':
+            dest = _gen_install_dest(chip_prod_base_dir, module_install_dir,
                                      relative_install_dir, module_type)
         dests.append(os.path.join(dest, source_file_name))
     return dests
@@ -113,6 +119,8 @@ def main():
     parser.add_argument('--ramdisk-base-dir', required=True)
     parser.add_argument('--vendor-base-dir', required=True)
     parser.add_argument('--updater-base-dir', required=True)
+    parser.add_argument('--sys_prod-base-dir', required=True)
+    parser.add_argument('--chip_prod-base-dir', required=True)
     parser.add_argument('--label-name', help='module name', required=True)
     parser.add_argument('--target-label', help='target label', required=True)
     parser.add_argument('--type', help='module type', required=True)
@@ -166,7 +174,7 @@ def main():
     if args.install_images:
         install_dests = gen_install_dests(
             args.system_base_dir, args.ramdisk_base_dir, args.vendor_base_dir,
-            args.updater_base_dir, source_file_name, args.install_images,
+            args.updater_base_dir, args.sys_prod_base_dir, args.chip_prod_base_dir, source_file_name, args.install_images,
             args.module_install_dir, args.relative_install_dir, args.type)
 
     module_info_data = gen_module_info(args.type, args.target_label,
