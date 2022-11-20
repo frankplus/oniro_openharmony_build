7a43b20c7835c0704afa7b3e1ee71e847c2a5d8a
lubinglun <lubinglun@huawei.com>
2022-11-20 05:16:40 +0000

    add simple rust_executable template  and dependance of musl for rust_target
    Signed-off-by: lubinglun <lubinglun@huawei.com>
    Change-Id: I95cac0d967ed3f0c9ea3f5797ca392ee11470f40

--- rust/hello_world/src/main.rs
+++ rust/hello_world/src/main.rs
@@ -1,13 +1,13 @@
-extern crate rust_log;
+// extern crate rust_log;
 
-use rust_log::RustLogMessage;
-use rust_log::rust_log;
+// use rust_log::RustLogMessage;
+// use rust_log::rust_log;
 
-fn main() {
-    let msg : RustLogMessage = RustLogMessage {id: 0, msg: "string in rlib crate".to_string()};
-    rust_log(msg);
-    println!("Hello, world!");
-}
 // fn main() {
-//     println!("hello, world!");
+//     let msg : RustLogMessage = RustLogMessage {id: 0, msg: "string in rlib crate".to_string()};
+//     rust_log(msg);
+//     println!("Hello, world!");
 // }
+fn main() {
+    println!("hello, world!");
+}
