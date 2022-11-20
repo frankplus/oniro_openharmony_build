c21c32d4d3dac04452a633a503db958d70377817
lubinglun <lubinglun@huawei.com>
2022-11-20 06:36:07 +0000

    convert rust_toolchain's abspath to relpath and add rust std rlib into build dir
    Signed-off-by: lubinglun <lubinglun@huawei.com>
    Change-Id: Ie6ef76fbb8257588940e34f71c7cfc5f234ad242

---
+++ rust/std/arm-unknown-linux-musleabi/deps/panic_abort-3d0fffebddbe77ec.d
@@ -0,0 +1,7 @@
+/mnt/lubinglun/test4/hello_world/target/arm-unknown-linux-musleabi/debug/deps/panic_abort-3d0fffebddbe77ec.rmeta: /mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
+
+/mnt/lubinglun/test4/hello_world/target/arm-unknown-linux-musleabi/debug/deps/libpanic_abort-3d0fffebddbe77ec.rlib: /mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
+
+/mnt/lubinglun/test4/hello_world/target/arm-unknown-linux-musleabi/debug/deps/panic_abort-3d0fffebddbe77ec.d: /mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
+
+/mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs:
---
+++ rust/std/arm-unknown-linux-musleabi/deps/rustc_std_workspace_core-19db6fe366b59349.d
@@ -0,0 +1,7 @@
+/mnt/lubinglun/test4/hello_world/target/arm-unknown-linux-musleabi/debug/deps/rustc_std_workspace_core-19db6fe366b59349.rmeta: /mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/mnt/lubinglun/test4/hello_world/target/arm-unknown-linux-musleabi/debug/deps/librustc_std_workspace_core-19db6fe366b59349.rlib: /mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/mnt/lubinglun/test4/hello_world/target/arm-unknown-linux-musleabi/debug/deps/rustc_std_workspace_core-19db6fe366b59349.d: /mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/mnt/lubinglun/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs:
