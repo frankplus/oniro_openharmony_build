9df4aabf2174be791306501cc8e28aa659223523
qy136 <18772452388@163.com>
2023-02-27 15:21:08 +0800

    add
    Signed-off-by: qy136 <18772452388@163.com>

--- ohos/images/build_image.py
+++ ohos/images/build_image.py
@@ -52,7 +52,7 @@ def _prepare_root(system_path, target_cpu):
 
 
 def _prepare_updater(updater_path):
-    _dir_list = ['dev', 'proc', 'sys', 'system', 'tmp', 'lib', 'lib64' ]
+    _dir_list = ['dev', 'proc', 'sys', 'system', 'tmp', 'lib', 'lib64']
     for _dir_name in _dir_list:
         _path = os.path.join(updater_path, _dir_name)
         if os.path.exists(_path):
@@ -63,6 +63,7 @@ def _prepare_updater(updater_path):
     os.symlink('/lib', os.path.join(updater_path, 'system/lib'))
     os.symlink('/lib64', os.path.join(updater_path, 'system/lib64'))
     os.symlink('/etc', os.path.join(updater_path, 'system/etc'))
+
 
 def _prepare_updater_vendor(updater_vendor_path):
     _dir_list = ['bin', 'lib', 'lib64']
