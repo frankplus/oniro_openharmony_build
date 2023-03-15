8c327c2ffd25fe57beb7443f3e2321fbfdc35e4b
lubinglun <lubinglun@huawei.com>
2023-03-15 15:12:46 +0000

    Change the method of linking clang libs
    Issue: https://gitee.com/openharmony/build/issues/I6LLVF
    
    Signed-off-by: lubinglun <lubinglun@huawei.com>
    Change-Id: I729ea93f5e4fb1f449a937716150ca92156d3845

--- templates/rust/run_build_script.py
+++ templates/rust/run_build_script.py
@@ -29,8 +29,8 @@ sys.path.append(
 from scripts.util import build_utils
 
 
-RUSTC_VERSION_LINE = re.compile(r"(\w+): (.*)")
-RUSTC_CFG_LINE = re.compile("cargo:rustc-cfg=(.*)")
+RUSTC_VERSION = re.compile(r"(\w+): (.*)")
+RUSTC_CFG = re.compile("cargo:rustc-cfg=(.*)")
 
 
 def host_triple(rustc_path):
@@ -39,14 +39,77 @@ def host_triple(rustc_path):
     args = [rustc_path, "-vV"]
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
     for lines in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
-        match = RUSTC_VERSION_LINE.match(lines.rstrip())
+        match = RUSTC_VERSION.match(lines.rstrip())
         if match:
             known_vars[match.group(1)] = match.group(2)
     return known_vars.get("host")
 
+def run_build_script(args, env, tempdir):
+    process = subprocess.run([os.path.abspath(args.build_script)],
+                                env=env,
+                                cwd=args.src_dir,
+                                encoding='utf8',
+                                capture_output=True)
+
+    if process.stderr.rstrip():
+        print(process.stderr.rstrip(), file=sys.stderr)
+    process.check_returncode()
+
+    flags = ""
+    for line in process.stdout.split("\n"):
+        match = RUSTC_CFG.match(line.rstrip())
+        if match:
+            flags = "%s--cfg\n%s\n" % (flags, match.group(1))
+
+    with build_utils.atomic_output(args.output) as output:
+        output.write(flags.encode("utf-8"))
+
+    if args.generated_files:
+        for generated_file in args.generated_files:
+            input_path = os.path.join(tempdir, generated_file)
+            output_path = os.path.join(args.out_dir, generated_file)
+            out_dir = os.path.dirname(output_path)
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+            with os.fdopen(os.open(input_path,
+                            os.O_RDWR | os.O_CREAT, stat.S_IWUSR | stat.S_IRUSR),
+                            'rb') as inputs:
+                with build_utils.atomic_output(output_path) as output:
+                    content = inputs.read()
+                    output.write(content)
+
+def set_env(args, rustc_path, tempdir):
+    env = {}  # try to avoid build scripts depending on other things
+    env["RUSTC"] = os.path.abspath(rustc_path)
+    env["HOST"] = host_triple(rustc_path)
+    env["CARGO_MANIFEST_DIR"] = os.path.abspath(args.src_dir)
+    env["OUT_DIR"] = tempdir
+
+    if args.target is not None:
+        env["TARGET"] = args.target
+    else:
+        env["TARGET"] = env.get("HOST")
+
+    target_components = env.get("TARGET").split("-")
+    env["CARGO_CFG_TARGET_ARCH"] = target_components[0]
+    if args.env:
+        for environment in args.env:
+            (key, value) = environment.split("=")
+            env[key] = value
+    if args.features:
+        for feature in args.features:
+            feature_name = feature.upper().replace("-", "_")
+            env["CARGO_FEATURE_%s" % feature_name] = "1"
+
+    # Pass through a couple which are useful for diagnostics
+    if os.environ.get("RUST_LOG"):
+        env["RUST_LOG"] = os.environ.get("RUST_LOG")
+    if os.environ.get("RUST_BACKTRACE"):
+        env["RUST_BACKTRACE"] = os.environ.get("RUST_BACKTRACE")
+    return env
 
 def main():
-    parser = argparse.ArgumentParser(description='Run Rust build script.')
+    parser = argparse.ArgumentParser()
     parser.add_argument('--build-script',
                         required=True,
                         help='build script needed to run')
@@ -65,72 +128,10 @@ def main():
                         help='ohos target source dir')
 
     args = parser.parse_args()
-    if platform.system() == "Windows":
-        rustc_path = os.path.join(args.rust_prefix, "rustc.exe")
-    else:
-        rustc_path = os.path.join(args.rust_prefix, "rustc")
-
+    rustc_path = os.path.join(args.rust_prefix, "rustc")
     with tempfile.TemporaryDirectory() as tempdir:
-        env = {}  # try to avoid build scripts depending on other things
-        env["RUSTC"] = os.path.abspath(rustc_path)
-        env["HOST"] = host_triple(rustc_path)
-        env["CARGO_MANIFEST_DIR"] = os.path.abspath(args.src_dir)
-        env["OUT_DIR"] = tempdir
-
-        if args.target is not None:
-            env["TARGET"] = args.target
-        else:
-            env["TARGET"] = env.get("HOST")
-
-        target_components = env.get("TARGET").split("-")
-        env["CARGO_CFG_TARGET_ARCH"] = target_components[0]
-        if args.env:
-            for environment in args.env:
-                (key, value) = environment.split("=")
-                env[key] = value
-        if args.features:
-            for feature in args.features:
-                feature_name = feature.upper().replace("-", "_")
-                env["CARGO_FEATURE_%s" % feature_name] = "1"
-
-        # Pass through a couple which are useful for diagnostics
-        if os.environ.get("RUST_LOG"):
-            env["RUST_LOG"] = os.environ.get("RUST_LOG")
-        if os.environ.get("RUST_BACKTRACE"):
-            env["RUST_BACKTRACE"] = os.environ.get("RUST_BACKTRACE")
-
-        process = subprocess.run([os.path.abspath(args.build_script)],
-                                 env=env,
-                                 cwd=args.src_dir,
-                                 encoding='utf8',
-                                 capture_output=True)
-
-        if process.stderr.rstrip():
-            print(process.stderr.rstrip(), file=sys.stderr)
-        process.check_returncode()
-
-        flags = ""
-        for line in process.stdout.split("\n"):
-            match = RUSTC_CFG_LINE.match(line.rstrip())
-            if match:
-                flags = "%s--cfg\n%s\n" % (flags, match.group(1))
-
-        with build_utils.atomic_output(args.output) as output:
-            output.write(flags.encode("utf-8"))
-
-        if args.generated_files:
-            for generated_file in args.generated_files:
-                input_path = os.path.join(tempdir, generated_file)
-                output_path = os.path.join(args.out_dir, generated_file)
-                out_dir = os.path.dirname(output_path)
-                if not os.path.exists(out_dir):
-                    os.makedirs(out_dir)
-                with os.fdopen(os.open(input_path,
-                               os.O_RDWR | os.O_CREAT, stat.S_IWUSR | stat.S_IRUSR),
-                               'rb') as inputs:
-                    with build_utils.atomic_output(output_path) as output:
-                        content = inputs.read()
-                        output.write(content)
+        env = set_env(args, rustc_path, tempdir)
+        run_build_script(args, env, tempdir)
 
 
 if __name__ == '__main__':
